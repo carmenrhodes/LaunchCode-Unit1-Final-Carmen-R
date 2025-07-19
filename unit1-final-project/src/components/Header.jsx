07efb8b84a96237d1fbe8461acbcaf5942900e66
CarmenRhodes <carmenrhodes289@gmail.com>
2025-07-18 22:06:44 -0500

    Created major components and all necessary page folders

---
+++ unit1-final-project/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <h1>TrackMyStack</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/stack">My Stack</a>
+        <a href="/add">Add Item</a>
+        <a href="/about">About</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ unit1-final-project/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <main>
+      <h2>Welcome to TrackMyStack</h2>
+      <p>This is your dashboard. Here you’ll see spot prices and inventory info.</p>
+    </main>
+  );
+}
+
+export default Home;
