1e73a410082b67c27960fa982b45dc172f9066a6
CarmenRhodes <carmenrhodes289@gmail.com>
2025-07-19 10:57:55 -0500

    Added responsive layout for mobile

--- unit1-final-project/src/components/Header.jsx
+++ unit1-final-project/src/components/Header.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+import { Sling as Hamburger } from 'hamburger-react';
+
 function Header() {
-  return (
-    <header className="sidebar">
-      <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-        <li><a href="/stack">My Stack</a></li>
-        <li><a href="/add">Add Item</a></li>
-        <li><a href="/about">About</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <header className="sidebar">
+                <div className="hamburger-wrapper">
+                    <Hamburger
+                        toggled={menuOpen}
+                        toggle={setMenuOpen}
+                        size={24}
+                        direction="left"
+                        label="Show menu"
+                        color="#2f5061"
+                    />
+                </div>
+
+            <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+                <h1>TrackMyStack</h1>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/stack">My Stack</a></li>
+                    <li><a href="/add">Add Item</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
